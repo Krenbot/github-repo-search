4c9304ea2337b58fb3cd60f4d1233ce0b0202164
Steven Krenn <stevekrenn@gmail.com>
2023-01-28 18:12:57 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import Navbar from './components/navbar'
+import SavedPage from './pages/saved';
+import SearchPage from './pages/search';
+
 function App() {
+  //view argument in usestate is default state
+  const [view, setView] = useState('search');
+
+  const renderView = () => {
+    if (view === 'search') {
+      return <SearchPage />
+    } else if (view = 'saved') {
+      return <SavedPage />
+    } else {
+      'No Matching View Found!'
+    }
+  };
+
+  console.log(view);
+
   return (
-    null
+    <>
+      <Navbar
+        view={view}
+        setView={setView}
+      />
+      {renderView()}
+    </>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,31 @@
-const Navbar = props => {
-    return 'Navbar'
+const Navbar = ({ view, setView }) => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary">
+            <div className="container">
+                <a className="navbar-brand" href="#">Repo Search</a>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a
+                            onClick={() => setView('search')}
+                            className={`nav-link ${view === 'search' ? 'active fw-bold' : ''}`}
+                            href="#"
+                        >
+                            <i className="bi bi-search-heart"></i> Search
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            onClick={() => setView('saved')}
+                            className={`nav-link ${view === 'saved' ? 'active fw-bold' : ''}`}
+                            href="#"
+                        >
+                            <i className="bi bi-bookmark-heart"></i> Saved
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
